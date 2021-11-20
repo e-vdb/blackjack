dde847bab4b56592cf1637b1e2631864b02511a2
evdb <emelinevanderbeken@gmail.com>
2021-11-20 19:05:52 +0100

    Initial commit

---
+++ blackjack/blackjack_classes.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Define classes.
+"""
+from blackjack_functions import calculate_hand
+import tkinter as tk
+import random
+
+
+colors=['coeur','carreau','trefle','pique']
+values=['as','2','3','4','5','6','7','8','9','10','valet','dame','roi']
+repCards=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+
+class Player(object):
+    def __init__(self):
+        self.hand = []
+    
+    def getHand(self):
+        return self.hand
+    
+    def add_card(self,card):
+        card_id = values.index(card.getValue())
+        self.hand.append(repCards[card_id])
+    
+    def cards_number(self):
+        return len(self.hand)
+    
+    def total_hand(self):
+        return calculate_hand(self.hand)
+    
+    def reinit(self):
+        self.hand = []
+
+
+class Card(object):
+    def __init__(self,val='as',col='carreau'):
+        self.value=val
+        self.color=col
+    
+    def getValue(self):
+        return self.value
+    
+    def display(self):
+        print("{} de {}".format(self.value,self.color))
+
+    def picture(self):
+        name='pictures/'+self.value+'_'+self.color+'.gif'
+        return tk.PhotoImage(file=name)
+
+
+class Pack52cards(object):
+    def __init__(self):
+        self.cards = []
+        for value in values:
+            for color in colors:
+                card = Card(value,color)
+                self.cards.append(card)
+        
+        
+    def shuffle_pack(self):
+        #random.seed(0)
+        random.shuffle(self.cards)
+        
+    def pick_card(self):
+        if len(self.cards)>0:
+            card=self.cards.pop(0)
+            return card
+        else:
+            return None
