fc9c24847454592fc1e9129a762eca2ba12eda55
evdb <emelinevanderbeken@gmail.com>
2021-11-22 14:14:37 +0100

    Fixed flake8 errors

--- blackjack/blackjack_classes.py
+++ blackjack/blackjack_classes.py
@@ -8,47 +8,60 @@ import tkinter as tk
 import random
 
 
-colors=['coeur','carreau','trefle','pique']
-values=['as','2','3','4','5','6','7','8','9','10','valet','dame','roi']
-repCards=['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+colors = ['coeur', 'carreau', 'trefle', 'pique']
+values = ['as', '2', '3', '4', '5', '6', '7',
+          '8', '9', '10', 'valet', 'dame', 'roi']
+repCards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
 
 class Player(object):
     """
-    Define a class to represent player/dealer.
+    A class to represent player/dealer.
+
+    Methods
+    -----------
+    add_card(card)
+
+    cards_number()
+
+    total_hand()
+
+    reinit()
+
     """
     def __init__(self):
         self.hand = []
-    
+
     def getHand(self):
         return self.hand
-    
-    def add_card(self,card):
+
+    def add_card(self, card):
         card_id = values.index(card.getValue())
         self.hand.append(repCards[card_id])
-    
+
     def cards_number(self):
         return len(self.hand)
-    
+
     def total_hand(self):
         return calculate_hand(self.hand)
-    
+
     def reinit(self):
         self.hand = []
 
 
 class Card(object):
     """
-    Define a class to represent a card.
+    A class to represent a card.
     """
-    def __init__(self,val='as', col='carreau'):
+    def __init__(self, val='as', col='carreau'):
         self.value = val
         self.color = col
-    
+
     def getValue(self):
         return self.value
-    
+
     def display(self):
-        print("{} de {}".format(self.value,self.color))
+        print("{} de {}".format(self.value, self.color))
 
     def picture(self):
         name = 'pictures/' + self.value + '_' + self.color + '.gif'
@@ -56,19 +69,21 @@ class Card(object):
 
 
 class Pack52cards(object):
+    """
+    A class to represent a pack of 52 cards.
+    """
     def __init__(self):
         self.cards = []
         for value in values:
             for color in colors:
-                card = Card(value,color)
+                card = Card(value, color)
                 self.cards.append(card)
-        
-        
+
     def shuffle_pack(self):
         random.shuffle(self.cards)
-        
+
     def pick_card(self):
-        if len(self.cards)>0:
+        if len(self.cards) > 0:
             card = self.cards.pop(0)
             return card
         else:
